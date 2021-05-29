0cbab1daf55a62c51b8db94bfa822489eff0a238
Kazuki Hara <yukidama.k@gmail.com>
2021-05-29 10:56:23 +0900

    fix

--- main.go
+++ main.go
@@ -12,10 +12,9 @@ import (
 	"github.com/gocarina/gocsv"
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"golang.org/x/text/unicode/norm"
 )
-
-const comitiaCircleListURL = "https://www.comitia.co.jp/history/134list_sp.html"
 
 type Circle struct {
 	Space  string `json:"space" csv:"space"`
@@ -117,13 +116,30 @@ func (p *Printer) JSON() error {
 	return nil
 }
 
+type Event struct {
+	CircleListURL string `mapstructure:"circle_list_url"`
+	Parser        string `mapstructure:"parser"`
+}
+
 func rootRun(cmd *cobra.Command, args []string) {
 	validate()
 
 	client := &http.Client{}
 
+	// Parse events
+	var events map[string]Event
+	if err := viper.UnmarshalKey("event", &events); err != nil {
+		log.Fatal(err)
+	}
+
+	// event key check
+	event, ok := events[args[0]]
+	if !ok {
+		log.Fatalf("no event key: %s", args[0])
+	}
+
 	// Request the HTML Page
-	resp, err := client.Get(comitiaCircleListURL)
+	resp, err := client.Get(event.CircleListURL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -149,9 +165,9 @@ func rootRun(cmd *cobra.Command, args []string) {
 
 	var appFs = afero.NewOsFs()
 
-	if format == "csv" {
+	if viper.GetString("format") == "csv" {
 		// Write CSV File
-		csvFile, err := appFs.OpenFile("test.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+		csvFile, err := appFs.OpenFile(viper.GetString("output"), os.O_RDWR|os.O_CREATE, os.ModePerm)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -164,9 +180,9 @@ func rootRun(cmd *cobra.Command, args []string) {
 		}
 	}
 
-	if format == "json" {
+	if viper.GetString("format") == "json" {
 		// Write JSON File
-		jsonFile, err := appFs.OpenFile("test.json", os.O_RDWR|os.O_CREATE, os.ModePerm)
+		jsonFile, err := appFs.OpenFile(viper.GetString("output"), os.O_RDWR|os.O_CREATE, os.ModePerm)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -184,20 +200,36 @@ var (
 	rootCmd = &cobra.Command{
 		Use:   "go-circle-list-extract",
 		Short: "go-circle-list-extract extracts a circle list of Comitia in JSON or CSV format.",
+		Args:  cobra.ExactArgs(1),
 		Run:   rootRun,
 	}
 
-	format string
+	cfgFile   string
+	eventName string
 )
 
 func validate() {
-	if format != "json" && format != "csv" {
+	if viper.GetString("format") != "json" && viper.GetString("format") != "csv" {
 		log.Fatal("invalid format")
 	}
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringVarP(&format, "format", "f", "csv", "output format")
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "config.yaml", "config file (default is config.yaml)")
+	rootCmd.PersistentFlags().StringVarP(&eventName, "event", "e", "comitia134", "event name")
+	rootCmd.PersistentFlags().StringP("format", "f", "csv", "output format")
+	rootCmd.PersistentFlags().StringP("output", "o", "circles.csv", "output file name")
+	viper.BindPFlag("format", rootCmd.PersistentFlags().Lookup("format"))
+	viper.BindPFlag("output", rootCmd.PersistentFlags().Lookup("output"))
+}
+
+func initConfig() {
+	viper.SetConfigFile(cfgFile)
+	if err := viper.ReadInConfig(); err == nil {
+		log.Println("Using config file:", viper.ConfigFileUsed())
+	}
 }
 
 func main() {
